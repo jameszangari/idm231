989539ce79ace3d9b0a3ad38b587eb925fe891c5
James Cliff <me@jamescliff.com>
2020-02-04 20:20:24 -0500

    changes

--- final_project/code/selector.js
+++ final_project/code/selector.js
@@ -5,39 +5,49 @@ function computeZod() {
   let userBday = zodDateObj.value;
   console.log('userBday is ' + userBday);
   // 1998-10-21
-  let monthStr = userBday.substr(5,2);
+  let monStr = userBday.substr(5,2);
   let dayStr = userBday.substr(8,2);
   // convert str into number
-  let monthNum = parseInt(monthStr);
+  let monNum = parseInt(monStr);
   let dayNum = parseInt(dayStr);
-  showMeObj.innerHTML = dateToZodiac(monthNum, dayNum);
+
+  console.log('monNum = ' + monNum);
+  console.log('dayNum = ' + dayNum);
+
+if (isNaN(monNum)) {
+    console.log('Please fill out this field.');
+  }
+
+  showMeObj.innerHTML = dateToZodiac(monNum, dayNum);
 }
 
 function dateToZodiac(whichMonth, whichDayOfMonth) {
   if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
-    AstroSign = "Capricorn";
+    AstroSign = "Capricorn // Mr. Poopybutthole";
   } else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
-    AstroSign = "Sagittarius";
+    AstroSign = "Sagittarius // Glootie";
   } else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
-    AstroSign = "Scorpio";
+    AstroSign = "Scorpio // Beebo";
   } else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
-    AstroSign = "Libra";
+    AstroSign = "Libra // Krombopulos Michael";
   } else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
-    AstroSign = "Virgo";
+    AstroSign = "Virgo // Jagoff";
   } else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
-    AstroSign = "Leo";
+    AstroSign = "Leo // King Flippy Nips";
   } else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
-    AstroSign = "Cancer";
+    AstroSign = "Cancer // Tony";
   } else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
-    AstroSign = "Gemini";
+    AstroSign = "Gemini // Morty Jr.";
   } else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
-    AstroSign = "Taurus";
+    AstroSign = "Taurus // Arthricia";
   } else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
-    AstroSign = "Aries";
+    AstroSign = "Aries // Chachi";
   } else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
-    AstroSign = "Pisces";
+    AstroSign = "Pisces // Gazorpazorpfield";
   } else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
-    AstroSign = "Aquarius";
+    AstroSign = "Aquarius // Zeep Xanflorp";
+  } else {
+    AstroSign = "Invalid entry"
   }
   return AstroSign;
 }
